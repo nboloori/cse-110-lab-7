9f1c040680d09301ff0875e208468061fe61f5f6
Navid Boloorian <navidboloorian0@gmail.com>
2022-11-09 16:08:38 -0800

    finished lab

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,17 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if('serviceWorker' in navigator) {
+    window.onload = () => {
+      navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service worker registration succeeded: ', registration);
+      },
+      (error) => {
+        console.log(`Service worker registration failed: ${error}`);
+      })
+    }
+  } 
 }
 
 /**
@@ -68,6 +79,9 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const existingRecipes = JSON.parse(localStorage.getItem('recipes'));
+
+  if(existingRecipes != null && existingRecipes.length > 0) return existingRecipes;
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
@@ -77,6 +91,28 @@ async function getRecipes() {
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  const fetchedRecipes = new Array();
+
+  return new Promise(async (resolve, reject) => {
+    for(let i = 0; i < RECIPE_URLS.length; i++) {
+      const recipeUrl = RECIPE_URLS[i];
+
+      try {
+        const response = await fetch(recipeUrl);
+        const recipe = await response.json();
+
+        fetchedRecipes.push(recipe);
+
+        if(i == RECIPE_URLS.length - 1) {
+          saveRecipesToStorage(fetchedRecipes);
+          resolve(fetchedRecipes);
+        }
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    }
+  });
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
@@ -121,6 +157,7 @@ function saveRecipesToStorage(recipes) {
 function addRecipesToDocument(recipes) {
   if (!recipes) return;
   let main = document.querySelector('main');
+
   recipes.forEach((recipe) => {
     let recipeCard = document.createElement('recipe-card');
     recipeCard.data = recipe;
